fd3b1959e88beea67be1489c2db7cef5b7d5a05c
Judzhin Miles <info@msbios.com>
2017-11-13 16:35:48 +0200

    Lazy Composer Commiter

--- config/module.config.php
+++ config/module.config.php
@@ -5,31 +5,54 @@
  */
 namespace MSBios\Viewer;
 
-
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
 
     'service_manager' => [
         'factories' => [
-            Module::class => Factory\ModuleFactory::class,
-            ViewerManager::class => Factory\ViewerManagerFactory::class,
-            ReaderManager::class => Factory\ReaderManagerFactory::class,
-            WriterManager::class => Factory\WriterManagerFactory::class,
+            Module::class =>
+                Factory\ModuleFactory::class,
+            ViewerManager::class =>
+                Factory\ViewerManagerFactory::class,
+            ReaderManager::class =>
+                Factory\ReaderManagerFactory::class,
+            WriterManager::class =>
+                Factory\WriterManagerFactory::class,
 
             // Resolvers
-            Resolver\ReaderSessionResolver::class => InvokableFactory::class,
-            Resolver\WriteSessionResolver::class => InvokableFactory::class,
+            Resolver\ReaderSessionResolver::class =>
+                InvokableFactory::class,
+            Resolver\WriteSessionResolver::class =>
+                InvokableFactory::class,
 
         ]
     ],
 
     Module::class => [
+
+        /**
+         * Reader resolvers.
+         *
+         * Expects: array
+         * Default: [
+         *     Resolver\ReaderSessionResolver::class => 100
+         * ]
+         */
         'reader_resolvers' => [
             Resolver\ReaderSessionResolver::class => 100
         ],
+
+        /**
+         * Writer resolvers.
+         *
+         * Expects: array
+         * Default: [
+         *     Resolver\WriteSessionResolver::class => 100
+         * ]
+         */
         'write_resolvers' => [
             Resolver\WriteSessionResolver::class => 100
         ]
-    ],
+    ]
 ];
